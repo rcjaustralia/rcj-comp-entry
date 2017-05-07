dbf1b2e4b75181a3ff56c48c7d2f170c814ac705
Tristan Roberts <t.roberts@uq.edu.au>
2017-05-07 11:40:30 +1000

    restructured for Docker file usage

--- rules/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-  header("location: /vic-rescue-rules/");
-?>
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-  echo '<html><p>';
-  echo strtr( 'It\'s raining rats and dogs' , 'rats' , 'cats' );
-  echo '</p></html>';
-?>
